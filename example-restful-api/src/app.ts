25f812a90271a91c1f727aa5291267aef94479d8
Danil Vagapov <adast@yahoo.com>
2019-11-04 19:19:41 +0500

    Added mongodb

--- example-restful-api/src/app.ts
+++ example-restful-api/src/app.ts
@@ -1,6 +1,19 @@
 import express from 'express';
+import { MONGODB_URI } from './util/secrets';
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
 
+// Create Express server
 const app = express();
+
+// Connect to MongoDB
+mongoose.Promise = bluebird;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
+    () => console.log("Connected to MongoDB")
+).catch(err => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    process.exit();
+});
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
